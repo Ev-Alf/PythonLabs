0471f3a025ccc3fbceeb4cb779addc779f194eb5
Ev-Alf <59028450+Ev-Alf@users.noreply.github.com>
2020-05-25 23:51:18 +0300

    Create lab2_#2.py

---
+++ lab22/lab2_#2.py
@@ -0,0 +1,10 @@
+import os
+from os.path import exists
+import hashlib
+import os
+import hashlib
+import random
+import re
+o = (str(os.listdir(path=".")))#.split()  #список файлов и директорий в папке
+print(hashlib.md5("Ex1E".encode('utf-8')).hexdigest())
+print(o)
